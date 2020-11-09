14d4061fcb8594edbbca7c44a18bf7490b01abf8
majhar-nayem <majharul.islam02@northsouth.edu>
2020-11-09 14:32:20 +0600

    author name inserted in version_2 ConsoleRunner.java

--- version_2/ConsoleRunner.java
+++ version_2/ConsoleRunner.java
@@ -4,7 +4,7 @@
  * ‘place’ methods in Game.  Prompts the user for inputs and outputs the state
  * of the board to the console.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Majharul Islam
  */
 
 import java.util.Scanner;
